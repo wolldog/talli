6e8ce2cc3c4024fe772a6d5d40443fe63c7e2559
Woll Dog <110208272+wolldog@users.noreply.github.com>
2023-05-01 15:24:17 +1000

    M. login/index.js; 1st draft login form

--- client/src/Pages/Login/index.js
+++ client/src/Pages/Login/index.js
@@ -1,10 +1,86 @@
 import React from "react";
 import { Typography } from "antd";
+import { Button, Checkbox, Form, Input } from "antd";
+import Auth from "../../utils/auth.js";
+
+const onFinish = (values) => {
+  console.log("Success:", values);
+};
+
+const onFinishFailed = (errorInfo) => {
+  console.log("Failed:", errorInfo);
+};
 
 const Login = () => {
   return (
-    <div className="login">
-      <Typography.Text>Login Form - You will only see this if you are not logged in.</Typography.Text>
+    <div>
+      <Typography.Title>Login</Typography.Title>
+      <Form
+        name="basic"
+        labelCol={{
+          span: 8,
+        }}
+        wrapperCol={{
+          span: 16,
+        }}
+        style={{
+          maxWidth: 600,
+        }}
+        initialValues={{
+          remember: true,
+        }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
+      >
+        <Form.Item
+          label="Username"
+          name="username"
+          rules={[
+            {
+              required: true,
+              message: "Please input your username!",
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: "Please input your password!",
+            },
+          ]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        {/* <Form.Item
+      name="remember"
+      valuePropName="checked"
+      wrapperCol={{
+        offset: 8,
+        span: 16,
+      }}
+    >
+      <Checkbox>Remember me</Checkbox>
+    </Form.Item> */}
+
+        <Form.Item
+          wrapperCol={{
+            offset: 8,
+            span: 16,
+          }}
+        >
+          <Button type="primary" htmlType="submit">
+            Login
+          </Button>
+        </Form.Item>
+      </Form>
     </div>
   );
 };
