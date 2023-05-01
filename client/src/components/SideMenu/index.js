c3f4ac713bdf8d3c5eb8fa8054f61d5b5b0dbaf5
Woll Dog <110208272+wolldog@users.noreply.github.com>
2023-05-01 18:32:46 +1000

    M. SideMenu.js; add conditional render

--- client/src/components/SideMenu/index.js
+++ client/src/components/SideMenu/index.js
@@ -5,29 +5,49 @@ import {
   TeamOutlined,
   UserOutlined,
   ClockCircleOutlined,
+  UnlockOutlined,
+  LoginOutlined,
 } from "@ant-design/icons";
 import { Menu } from "antd";
 
+import Auth from "../../utils/auth.js";
+
 const SideMenu = () => {
   const navigate = useNavigate();
+
   return (
     <div className="SideMenu">
-      <Menu
-        onClick={(item) => {
-          navigate(item.key);
-        }}
-        items={[
-          { label: "Home", icon: <ClockCircleOutlined />, key: "/" },
-          {
-            label: "Dashboard",
-            icon: <DashboardOutlined />,
-            key: "/dashboard",
-          },
-          { label: "Groups", icon: <TeamOutlined />, key: "/groups" },
-          { label: "Friends", icon: <UserOutlined />, key: "/friends" },
-          
-        ]}
-      ></Menu>
+      {Auth.loggedIn() ? (
+        <Menu
+          onClick={(item) => {
+            navigate(item.key);
+          }}
+          items={[
+            { label: "Home", icon: <ClockCircleOutlined />, key: "/" },
+            {
+              label: "Dashboard",
+              icon: <DashboardOutlined />,
+              key: "/dashboard",
+            },
+            { label: "Groups", icon: <TeamOutlined />, key: "/groups" },
+            { label: "Friends", icon: <UserOutlined />, key: "/friends" },
+          ]}
+        ></Menu>
+      ) : (
+        <Menu
+          onClick={(item) => {
+            navigate(item.key);
+          }}
+          items={[
+            { label: "Login", icon: <UnlockOutlined />, key: "/login" },
+            {
+              label: "Join",
+              icon: <LoginOutlined />,
+              key: "/join",
+            },
+          ]}
+        ></Menu>
+      )}
     </div>
   );
 };
