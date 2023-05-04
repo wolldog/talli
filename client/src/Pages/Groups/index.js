19d7d4ef396a13c5651d4ea5464ac5b46ad207e7
Woll Dog <110208272+wolldog@users.noreply.github.com>
2023-05-04 15:56:59 +1000

    M. Groups/index.js; Add group form and function

--- client/src/Pages/Groups/index.js
+++ client/src/Pages/Groups/index.js
@@ -1,14 +1,29 @@
-import { useState } from "react";
-import { useQuery } from "@apollo/client";
+import { useState, useEffect } from "react";
+import { useQuery, useMutation } from "@apollo/client";
+import { Link } from 'react-router-dom'
 import {
   EditOutlined,
   EllipsisOutlined,
   SettingOutlined,
 } from "@ant-design/icons";
-import { Avatar, Card, Space, Divider, Button, Row, Col, Modal } from "antd";
-import { QUERY_USERS_GROUPS } from "../../utils/queries.js";
 
-import { Typography } from "antd";
+import {
+  Avatar,
+  Card,
+  Space,
+  Divider,
+  Button,
+  Row,
+  Col,
+  Modal,
+  Input,
+  Select,
+  Form,
+  Typography,
+} from "antd";
+
+import { QUERY_USERS_GROUPS } from "../../utils/queries.js";
+import { ADD_GROUP } from "../../utils/mutations.js";
 
 const { Meta } = Card;
 
@@ -18,6 +33,48 @@ const Groups = () => {
 
   //Declare variable 'groups' to hold retrieved data.
   const groups = data?.groups || {};
+  console.log(groups);
+
+  const [addGroup, { error }] = useMutation(ADD_GROUP);
+
+  const handleAddGroup = async () => {
+    try {
+      const { data } = await addGroup({
+        variables: { ...formState },
+      });
+
+      setOpen(false);
+      setFormState({ groupname: "" });
+    } catch (err) {
+      if(err) {
+
+      };
+    }
+  };
+
+  const [formState, setFormState] = useState({
+    groupname: "",
+  });
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setFormState({
+      [name]: value,
+    });
+  };
+
+  //Modal
+
+  const [open, setOpen] = useState(false);
+
+  const showModal = () => {
+    setOpen(true);
+  };
+
+  const [confirmLoading, setConfirmLoading] = useState(false);
+
+  // const [modalText, setModalText] = useState("Content of the modal");
 
   if (loading) {
     return <div>Loading...</div>;
@@ -32,14 +89,56 @@ const Groups = () => {
           </Col>
         </Row>
       ) : (
-        <> </>
+       null
       )}
 
-      
+      <Row justify="center">
+        <Col span={4}>
+          <Button type="primary" onClick={showModal}>
+            Add a group
+          </Button>
+          {error ? (
+          <div>
+            <p className="error-text">The provided credentials are incorrect</p>
+          </div>
+        ) : null}
+        </Col>
+      </Row>
+
+      <div>
+        <Modal
+          title="Add a group"
+          open={open}
+          onOk={({ target }) => {
+            setConfirmLoading();
+            console.log(target);
+            handleAddGroup(target);
+            setFormState({ groupname: "" });
+            setOpen(false);
+          }}
+          okText="Add Group"
+          confirmLoading={confirmLoading}
+          onCancel={() => {
+            setOpen(false);
+          }}
+        >
+          <Input
+            placeholder="Enter your group name"
+            className="modalInput"
+            name="groupname"
+            type="text"
+            value={formState.groupname}
+            onChange={handleChange}
+          ></Input>
+        </Modal>
+      </div>
+
+      <Divider />
       <Space wrap>
         {groups &&
-          groups.map((group, index) => (
-            <div className="groupCard" key={index}>
+          groups.map((group) => (
+            <div className="groupCard" key={group._id}>
+              
               <Card
                 style={{
                   width: 300,
@@ -63,6 +162,7 @@ const Groups = () => {
                   title={group.groupname}
                 />
               </Card>
+             
             </div>
           ))}
       </Space>
