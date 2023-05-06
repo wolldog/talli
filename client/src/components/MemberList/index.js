137dc1a6aa440e4f037d9596c8143dd3691ca80d
Woll Dog <110208272+wolldog@users.noreply.github.com>
2023-05-06 14:10:27 +1000

    M. MemberList/index; A. Add member button A. Remove member link

--- client/src/components/MemberList/index.js
+++ client/src/components/MemberList/index.js
@@ -1,54 +1,57 @@
-import React from 'react';
+import React from "react";
 import {
-    Avatar,
-    Card,
-    Space,
-    Divider,
-    Button,
-    Row,
-    Col,
-    Modal,
-    Input,
-    Typography,
-    List
-  } from "antd";
+  Avatar,
+  Card,
+  Space,
+  Divider,
+  Button,
+  Row,
+  Col,
+  Modal,
+  Input,
+  Typography,
+  List,
+} from "antd";
 
-const MemberList = ({ members = []}) => {
-    if (!members.length) {
-        return <Typography.Text>You are looking a bit lonely, add some friends to your group</Typography.Text>
-    }
-
-
-
+const MemberList = ({ members = [] }) => {
+  if (!members.length) {
     return (
-        <div className='memberList' style={{width: 500}}>
+      <Typography.Text>
+        You are looking a bit lonely, add some friends to your group
+      </Typography.Text>
+    );
+  }
 
-           <List
-          size="large"
-          header={<div>Group Members</div>}
-          
-         
-          
-          
-    dataSource={members}
-    bordered={true}
-   
-    renderItem={(item, index) => (
-      <List.Item>
-        <List.Item.Meta
-          avatar={<Avatar src={`https://xsgames.co/randomusers/avatar.php?g=pixel&key=${index}`} />}
-          title={<a href="https://ant.design">{item.nickname}</a>}
-          description={item.email}
-        />
-      </List.Item>
-    )}
-  />
-                </div>
-
-    )
-
-
-
-}
+  return (
+    <div className="memberList" style={{ width: 500 }}>
+      <List
+        size="large"
+        header={<div>Group Members</div>}
+        footer={
+          <div>
+            <Button>Add Member</Button>
+          </div>
+        }
+        dataSource={members}
+        bordered={true}
+        renderItem={(item, index) => (
+          <List.Item
+            actions={[<a key="list-loadmore-edit">Remove from group</a>]}
+          >
+            <List.Item.Meta
+              avatar={
+                <Avatar
+                  src={`https://xsgames.co/randomusers/avatar.php?g=pixel&key=${index}`}
+                />
+              }
+              title={<a href="https://ant.design">{item.nickname}</a>}
+              description={item.email}
+            />
+          </List.Item>
+        )}
+      />
+    </div>
+  );
+};
 
 export default MemberList;
