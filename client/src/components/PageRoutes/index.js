0e42ffe3569234e0f1ecb9654c7d88b3d15fc936
Woll Dog <110208272+wolldog@users.noreply.github.com>
2023-05-08 18:00:55 +1000

    Remove Dashboard page

--- client/src/components/PageRoutes/index.js
+++ client/src/components/PageRoutes/index.js
@@ -1,7 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import Join from "../../Pages/Join";
 import Home from "../../Pages/Home";
-import Dashboard from "../../Pages/Dashboard";
 import Login from "../../Pages/Login";
 import Groups from "../../Pages/Groups";
 import SingleGroup from "../../Pages/SingleGroup";
@@ -10,7 +9,7 @@ const PageRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/dashboard/" element={<Dashboard />}></Route>
+      {/* <Route path="/dashboard/" element={<Dashboard />}></Route> */}
       <Route path="/login" element={<Login />}></Route>
       <Route path="/join" element={<Join />}></Route>
       <Route path="/groups" element={<Groups />}></Route>
